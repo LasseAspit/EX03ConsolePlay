48e7e39b010a49acdab2ee09eaedc73b39780eb6
3kant50075 <3kant50075@CV-BB-5858>
2021-06-02 10:51:01 +0200

    lommeregner done

---
+++ EX05Arithmetic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EX05Arithmetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("velkommen til lommeregner v1 \nvenligst intast de 2 numre du vil have lagt sammen\n");
+            Console.Write("intast tal 1: ");
+            double tal1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("intast tal 2: ");
+            double tal2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"\nresultatet af at lægge de to tal sammen er: {tal1 + tal2}");
+            Console.WriteLine($"resultatet af at trække de to tal fra hinanden er: {tal1 - tal2}");
+            Console.WriteLine($"resultatet af at gange de to tal er: {tal1 * tal2}");
+            Console.WriteLine($"resultatet af at dividere de to tal er : {tal1 / tal2}");
+            Console.WriteLine($"resultatet af at modulo de to tal er : {tal1 % tal2}");
+
+
+        }
+    }
+}
