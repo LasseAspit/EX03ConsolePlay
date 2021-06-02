48299931105fab91dacfccd87a62f7e0dc8684b7
3kant50075 <3kant50075@CV-BB-5858>
2021-06-02 10:58:49 +0200

    opg6

---
+++ EX06ComparisonOperators/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EX06ComparisonOperators_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 2;
+            int b = 4;
+            Console.WriteLine("are the two values different?");
+            Console.WriteLine(a!=b);
+            Console.WriteLine("is a bigger than b?");
+            Console.WriteLine(a > b);
+            Console.WriteLine("is b bigger or equal to a");
+            Console.WriteLine(a <= b);
+            Console.WriteLine("is a+b bigger than 0");
+            Console.WriteLine(a+b>0);
+            Console.WriteLine("is a bigger than 1-b");
+            Console.WriteLine(a> 1-b);
+        }
+    }
+}
