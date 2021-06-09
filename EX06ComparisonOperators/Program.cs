9ec7b692184a902648d4d00456b91f517312a24e
3kant50075 <3kant50075@CV-BB-5858>
2021-06-09 13:05:16 +0200

    opg 11

--- EX06ComparisonOperators/Program.cs
+++ EX06ComparisonOperators/Program.cs
@@ -7,18 +7,32 @@ namespace EX06ComparisonOperators_
         static void Main(string[] args)
         {
 
-            int a = 2;
-            int b = 4;
-            Console.WriteLine("are the two values different?");
-            Console.WriteLine(a!=b);
-            Console.WriteLine("is a bigger than b?");
-            Console.WriteLine(a > b);
-            Console.WriteLine("is b bigger or equal to a");
-            Console.WriteLine(a <= b);
-            Console.WriteLine("is a+b bigger than 0");
-            Console.WriteLine(a+b>0);
-            Console.WriteLine("is a bigger than 1-b");
-            Console.WriteLine(a> 1-b);
+            string exit = "haha";
+
+            while (exit != "exit")
+            {
+
+
+
+                int a = 2;
+                int b = 4;
+                Console.WriteLine("are the two values different?");
+                Console.WriteLine(a != b);
+                Console.WriteLine("is a bigger than b?");
+                Console.WriteLine(a > b);
+                Console.WriteLine("is b bigger or equal to a");
+                Console.WriteLine(a <= b);
+                Console.WriteLine("is a+b bigger than 0");
+                Console.WriteLine(a + b > 0);
+                Console.WriteLine("is a bigger than 1-b");
+                Console.WriteLine(a > 1 - b);
+
+
+                Console.WriteLine("type 'exit' to exit the program, or anything else to continue");
+                exit = Console.ReadLine();
+
+
+            }
         }
     }
 }
---
+++ EX11While/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace EX11While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 1;
+            
+
+            while (i<=10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            Console.WriteLine("\n");
+
+            i = 10;
+
+            while (i >= 1)
+            {
+                Console.WriteLine(i);
+                i--;
+            }
+
+            Console.WriteLine("\n");
+
+            i = 1;
+
+            while (i <= 10)
+            {
+                Console.WriteLine(i * 2);
+                i++;
+            }
+
+            i = 1;
+
+            Console.WriteLine("\n");
+
+            while (i <= 20)
+            {
+                Console.WriteLine(i);
+                i = i + 2;
+            }
+
+            Console.WriteLine("\n");
+            i = 1;
+
+            Console.WriteLine("indtast nummeret på den tabel du vil have");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            while (i <= 10)
+            {
+                Console.WriteLine(i * x);
+                i++;
+            }
+
+            i = 1;
+            x = 1;
+
+
+
+
+
+            while (i <= 1 || i > 10)
+            {
+                Console.WriteLine("indtast et nummer mellem 1 og 10");
+                i = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
