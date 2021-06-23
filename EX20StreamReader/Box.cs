2c2bf9e90aeaf6e4b990c86fa1cbc642e9159afd
3kant50075 <3kant50075@CV-BB-5858>
2021-06-23 11:52:56 +0200

    opg20

---
+++ EX20StreamReader/Box.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX20StreamReader_
+{
+    class Box
+    {
+        public int Højde { get; set; }
+        public int Længde { get; set; }
+        public int Bredde { get; set; }
+        private int Volume { get; set; }
+        private int Surface { get; set; }   
+
+
+        public Box(int højde, int længde, int bredde)
+        {
+            Højde = højde;
+            Længde = længde;
+            Bredde = bredde;
+            int volume;
+        }
+
+        public void VolumeCalc()
+        {
+            Volume = Højde * Længde * Bredde;
+        }
+
+
+        public void PrintInfo()
+        {
+            SurfaceCalc();
+            VolumeCalc();
+            Console.WriteLine($"Kassen har følgende mål: \nhøjde: {Højde}\nlængde: {Længde}\nbredde: {Bredde}\nVolume: {Volume}\noverflade: {Surface}");
+        }
+
+        public void SurfaceCalc()
+        {
+            Surface = (Højde * Bredde)*2 + (Længde * Bredde)*2 + (Længde * Højde)* 2;
+        }
+
+
+
+    }
+}
--- EX20StreamReader/Program.cs
+++ EX20StreamReader/Program.cs
@@ -1,12 +1,112 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace EX20StreamReader_
 {
     class Program
     {
+            
+
+       
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            static List<double> getNumbers(string url)
+            {
+                List<double> longList = new List<double>();
+
+                using (StreamReader sr = new StreamReader(url))
+                {
+
+                    string line;
+
+                    while (sr.EndOfStream != true)
+                    {
+                        line = sr.ReadLine();
+
+                        double lineDouble = Convert.ToDouble(line);
+                        longList.Add(lineDouble);
+
+
+                        /*
+                        counter++;
+                        sum = sum + Convert.ToDouble(line);
+                        */
+                    }
+
+
+
+                }
+
+
+
+
+                return longList;
+            }
+
+
+
+
+            /*
+
+
+            double sum = 0;
+            double counter = 0;
+
+            List<double> values = new List<double>();
+            
+
+            using (StreamReader sr = new StreamReader("Values.txt"))
+            {
+
+                string line;
+
+                while(sr.EndOfStream != true)
+                {
+                    line = sr.ReadLine();
+                    Console.WriteLine(line);
+
+                    double lineDouble = Convert.ToDouble(line);
+                    values.Add(lineDouble);
+
+
+                    
+                    counter++;
+                    sum = sum + Convert.ToDouble(line);
+                    
+                }
+
+
+
+            }
+            
+            Console.WriteLine("");
+            Console.WriteLine($"Summen er: {sum}");
+            Console.WriteLine($"Gennemsnittet er: {sum / counter}");
+            
+
+            foreach(double value in values)
+            {
+                sum = sum + value;
+                counter++;
+            }
+            Console.WriteLine(sum);
+            Console.WriteLine(sum /counter);
+
+            */
+
+
+            List<double> list = getNumbers("Values.txt");
+
+            foreach(double value in list)
+            {
+                Console.WriteLine(value);
+            }
+
+
+
         }
     }
 }
